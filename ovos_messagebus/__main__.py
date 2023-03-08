7958119912441a79d06684927603250044e7b3d0
jarbasai <jarbasai@mailfence.com>
2023-03-08 22:23:56 +0000

    bump ovos utils

--- ovos_messagebus/__main__.py
+++ ovos_messagebus/__main__.py
@@ -41,6 +41,7 @@ def on_stopping():
 
 
 def main(ready_hook=on_ready, error_hook=on_error, stopping_hook=on_stopping):
+    PIDLock.init()
     reset_sigint_handler()
     init_service_logger("bus")
     PIDLock("bus")
